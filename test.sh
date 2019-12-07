ebb0b49508c48ba0c6bbec4986802be34435af87
James Peach <james_peach01@hotmail.co.uk>
2019-12-07 23:40:44 +0000

    added testing script

---
+++ test.sh
@@ -0,0 +1,42 @@
+
+echo -e '\e[2J'
+echo clear screen
+echo clear screen
+echo clear screen
+echo clear screen
+echo clear screen
+echo clear screen
+echo clear screen
+sleep 1
+echo -e '\e[3A     above'
+sleep 1
+echo -e '\e[J'
+echo after erase below
+
+echo hiden
+echo -e 'dlrow\e[1;Fhello'
+echo world
+
+echo
+echo -------
+echo
+
+echo hiden
+echo -e 'dlrow\e[1;Ehello'
+echo world
+
+echo
+echo -------
+echo
+
+echo -e '1234\e[GABCD'
+
+echo
+echo -------
+echo 
+
+#echo -e "\e[1;10HIIII"
+#echo -e "\e[2;10H|  |"
+#echo -e "\e[3;10HIIII"
+
+
